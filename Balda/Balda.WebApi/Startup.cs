67fb4cb84c510b1b5c763bd83cccd1bf80f8c090
Jan Iwanow <iwanow.jan@gmail.com>
2021-02-18 09:21:47 +0700

    setup identity and its db context

--- Balda/Balda.WebApi/Startup.cs
+++ Balda/Balda.WebApi/Startup.cs
@@ -1,7 +1,12 @@
+using Balda.WebApi.Database;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 
 namespace Balda.WebApi
@@ -19,7 +24,27 @@ namespace Balda.WebApi
         // Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            ConfigureDatabase(services);
+            ConfigureIdentity(services);
+            
             services.AddControllers();
+        }
+
+        private void ConfigureDatabase(IServiceCollection services)
+        {
+            services.AddDbContext<BaldaUserDbContext>(
+                opt => opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"))
+            );
+        }
+
+        private static void ConfigureIdentity(IServiceCollection services)
+        {
+            services.TryAddSingleton<ISystemClock, SystemClock>();
+            
+            var builder = services.AddIdentityCore<BaldaUser>();
+            var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
+            identityBuilder.AddEntityFrameworkStores<BaldaUserDbContext>();
+            identityBuilder.AddSignInManager<SignInManager<BaldaUser>>();
         }
 
         // This method gets called by the runtime.
