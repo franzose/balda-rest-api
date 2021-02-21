d3c7fac3e79e2d253d84462dfd674a365a6ac84b
Jan Iwanow <iwanow.jan@gmail.com>
2021-02-21 19:41:27 +0700

    configure logging (simple for now)

--- Balda/Balda.WebApi/Program.cs
+++ Balda/Balda.WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Balda.WebApi
 {
@@ -12,6 +13,11 @@ namespace Balda.WebApi
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
